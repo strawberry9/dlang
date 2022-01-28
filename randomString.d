7f0a9013857c60750798f565349010106425023b
strawberry9 <strawberry9@users.noreply.github.com>
2022-01-28 18:44:52 +1100

    Create randomString.d

---
+++ randomString.d
@@ -0,0 +1,19 @@
+// Distributed under the FREE_FOR_ANY_USE LICENCE Version 1.0.
+
+module test;
+
+import std.stdio : writeln;
+import std.algorithm : map;
+import std.ascii : letters;
+import std.random : Random, unpredictableSeed, uniform;
+import std.range : iota, array;
+
+Random rnd;
+static this() {  rnd = Random(unpredictableSeed); } 
+
+void main()
+{
+    int howMany = 12;
+    string charsToUse = letters ~ "!#$%" ~ "0123456789";
+    writeln(iota(howMany).map!(_ => charsToUse[uniform(0, $, rnd)]).array);
+}
