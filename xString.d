8c4d434dd17e9318f375bbd9238ed8b101ea529c
strawberry9 <strawberry9@users.noreply.github.com>
2022-01-28 19:33:11 +1100

    Update xString.d

--- xString.d
+++ xString.d
@@ -1,7 +1,7 @@
 module test;
 @safe:
 
-import std.stdio : write;
+import std.stdio : write, writeln;
 import std.range : iota, array;
 import std.algorithm : map;
 
@@ -9,8 +9,10 @@ void main()
 {
     int n = 10;
     
-    // create a string containing n "x"s
+    // create a string array containing n "x"s
     auto xString = iota(0, n).map!(_ => "x" ).array; 
+    
+    writeln("xString is a ", typeof(xString).stringof); // xString is a string[]
     
     foreach(el; xString)
         write(el);
