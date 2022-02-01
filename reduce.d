33b5d292a10612f97774fcce9296c7f119fbbc62
strawberry9 <strawberry9@users.noreply.github.com>
2022-02-01 13:32:28 +1100

    Create reduce.d

---
+++ reduce.d
@@ -0,0 +1,15 @@
+module test;
+@safe:
+
+import std;
+
+void main()
+{
+    static int[5] arr = [5, 1, 5, 5, 1];
+    
+    // This example passes in a string as a template argument
+    writeln(arr.reduce!`a + b`); // 17
+
+    // Or, using a typical lambda expression
+    writeln(arr.reduce!((a,b) => (a + b))); // 17
+}
