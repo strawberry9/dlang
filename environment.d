71c7c56a0a4d36901d786bffc99e96f781ef46f2
strawberry9 <strawberry9@users.noreply.github.com>
2022-02-01 12:27:50 +1100

    Update environment.d

--- environment.d
+++ environment.d
@@ -5,14 +5,18 @@ import std.stdio : writeln, writefln;
 import std.process : environment;
 
 // This searches your environment variables for 'key'
-// and writes its value out to the console.
 
 void main()
 {
     string key = "DLANG_BACKTRACE";
     
     if (key in environment)
-        writeln(environment[key]);
+    {
+        writefln("%s=%s", key, environment[key]);
+        
+        if (environment[key] != "1")
+            writefln("set %s=1  (if you want to see stack trace output)", key);
+    }
     else
         writefln("%s not found.", key);
                
