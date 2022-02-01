cdea235286ab53f7254a9c72f2efbd8e1dd53abc
strawberry9 <strawberry9@users.noreply.github.com>
2022-02-01 13:44:26 +1100

    Create filter.d

---
+++ filter.d
@@ -0,0 +1,26 @@
+module test;
+@safe:
+
+import std;
+
+void main()
+{
+    string[] strings = [ "one", "", "two", "", "", "three", ""];
+    writeln(strings.length); // 7
+
+    strings= strings.filter!(a => !a.empty).array;
+    writeln(strings.length); // 3
+    
+    foreach(str; strings)
+        writeln(str);
+}
+
+/+
+
+7
+3
+one
+two
+three
+
++/
